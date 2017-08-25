e276fbd9dd05aa34463d0100fb17b828e351e90e
XB <xbisatrouble@gmail.com>
2017-08-25 11:01:53 +0800

    first commit

---
+++ src/shortUrlServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Xbb\shortUrl;
+
+use Illuminate\Support\ServiceProvider;
+
+class shortUrlServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../database/migrations/' => database_path('migrations')
+        ], 'migrations');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('shortUrl',function (){
+            return new shortUrl();
+        });
+    }
+}
